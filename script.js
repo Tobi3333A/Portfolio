0d4c771f19b3b1600fbba4484ea4f5b3bdc3949d
Oluwatobiloba Emmanuel Adejumo <oadejumo@xula.edu>
2025-11-19 20:24:17 -0600

    Updated whole resume

---
+++ script.js
@@ -0,0 +1,7 @@
+$("#drop").click(function() {
+    if ($(".down").css("display") === "none") {
+        $(".down").css("display", "block");
+    } else {
+        $(".down").css("display", "none");
+    }
+});
